91bb4b6ffdaa61edcee9341e74f9cacbce02ae7f
rberghout <rachelberghout@mail.weber.edu>
2017-10-04 09:20:05 -0600

    Modified login modal to be centered Added opacity to the background

--- Youth-Futures-Shelter-Home/wwwroot/js/login.js
+++ Youth-Futures-Shelter-Home/wwwroot/js/login.js
@@ -1,4 +1,9 @@
 ﻿var modal = document.getElementById('loginScn');
+var btn = document.getElementById('cncl');
+
+btn.onclick = function (event) {
+        modal.style.display = 'none'
+}
 
 window.onclick = function (event) {
     if (event.target == modal) {
