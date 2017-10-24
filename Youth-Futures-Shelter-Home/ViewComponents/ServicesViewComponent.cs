837181e56e1cb16508272a6fe8f17112407251ab
scotthadzik <smhadzik@parimote.com>
2017-10-23 22:47:55 -0600

    Added service section to display based on loop instead of static

--- Youth-Futures-Shelter-Home/ViewComponents/ServicesViewComponent.cs
+++ Youth-Futures-Shelter-Home/ViewComponents/ServicesViewComponent.cs
@@ -15,8 +15,9 @@ namespace Youth_Futures_Shelter_Home.ViewComponents
             {
                 var services = new List<ServicesItem>()
                 {
-                    new ServicesItem() { Title = "s1", Description = "d1"},
-                    new ServicesItem() { Title = "s2", Description = "d2"}
+                    new ServicesItem() { Image = "/images/house_icon.png", Alt = "House", Title = "Overnight Shelter", Description = "Located in the heart of downtown Ogden, Utah, Youth Futures provides emergency shelter, temporary residence and supportive services for runaway, homeless, unaccompanied and at-risk youth ages 12-17. The shelter is open 24 hours per day."},
+                    new ServicesItem() { Image = "/images/door_icon.png", Alt = "Door", Title = "Drop-in Services", Description = "Available to any youth ages 12-18. Drop-in services allow for the youth to access food, clothing, hygiene items, laundry facilities, computer stations, and case management. Drop-in hours are 6:30 am to 8:00 pm every day of the week."},
+                    new ServicesItem() { Image = "/images/van_icon.png", Alt = "Van", Title = "Street Outreach", Description = "Youth Futures’ Street Outreach is conducted once per week and provides outreach and crisis services to youth in Ogden City, Utah."},
                 };
 
                 return View(services);
@@ -35,6 +36,8 @@ namespace Youth_Futures_Shelter_Home.ViewComponents
 
         public string Description { get; set; }
         public string Title { get; set; }
+        public string Image { get; set; }
+        public string Alt { get; set; }
     }
 
     
